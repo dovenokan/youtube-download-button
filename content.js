f1da68e15aeef330352709f1a7e028975ef658ca
odd <42724589+dovenokan@users.noreply.github.com>
2021-02-05 16:01:32 +0300

    Update content.js

--- content.js
+++ content.js
@@ -1,17 +1,20 @@
-
+var node = document.createElement("a");
 
 function func() {
-	var link = window.location.href.replace("youtube","youtubepp"); 
-	var node = document.createElement("a");
 	var textnode = document.createTextNode("DOWNLOAD");
 	var att = document.createAttribute("class");       
 	att.value = "odytmp4";
-	node.href = link
+	node.href = "https://www.youtube.com/channel/UCpryVRk_VDudG8SHXgWcG0w"
 	node.target = "_blank"
 	node.setAttributeNode(att);
 	node.appendChild(textnode);
 	document.getElementById("owner-sub-count").appendChild(node);
 };
 
+function genLink() {
+	node.href = window.location.href.replace("youtube","youtubepp"); 
+	console.log(node.href)
+};
 
+setInterval(genLink, 500);
 window.onload = func;
