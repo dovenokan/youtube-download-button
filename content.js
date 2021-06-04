422c4b2a1cdd60408f80c30b08090157b17f68a2
odd <42724589+dovenokan@users.noreply.github.com>
2021-06-04 18:41:36 +0300

    Update content.js

--- content.js
+++ content.js
@@ -2,18 +2,22 @@ var node = document.createElement("a");
 var textnode = document.createTextNode("DOWNLOAD");
 var att = document.createAttribute("class");   
 
+
 function func() {   
 	att.value = "odytmp4";
 	node.href = "https://www.youtube.com/channel/UCpryVRk_VDudG8SHXgWcG0w"
 	node.target = "_blank"
 	node.setAttributeNode(att);
 	node.appendChild(textnode);
-	document.getElementById("owner-sub-count").appendChild(node);
+	document.querySelectorAll('#owner-sub-count')[1].appendChild(node);
 };
 
 function genLink() {
 	node.href = window.location.href.replace("youtube","youtubepp"); 
+	console.log(node)
 };
 
-setInterval(genLink, 1500);
-window.onload = func;
+
+
+setInterval(genLink, 500);
+window.onload = setTimeout(func, 3000);
